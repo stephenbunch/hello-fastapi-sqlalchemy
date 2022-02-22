b8cd3d97d964c1b57b677971c3d1b8cd7ce2846b
Stephen Bunch <bunchclone@gmail.com>
2022-02-21 23:34:56 -0800

    Add session class

--- db.py
+++ db.py
@@ -2,10 +2,14 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy import Column, Integer, Boolean, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
-db = create_engine(os.environ["DATABASE_URL"])
+engine = create_engine(os.environ["DATABASE_URL"])
+
 Base = declarative_base()
+
+Session = sessionmaker(bind=engine)
 
 
 class Todo(Base):
@@ -17,4 +21,4 @@ class Todo(Base):
 
 
 if __name__ == "__main__":
-    Base.metadata.create_all(db)
+    Base.metadata.create_all(engine)
